7a50c0b03183bcd4d3d6114f8d96825c4908ca56
skasegarmail-gif <skasegar.mail@gmail.com>
2026-05-31 21:12:17 +0330

    Improve guess game

--- guess_game.py
+++ guess_game.py
@@ -1,1 +1,15 @@
-print("Hello, GitHub!")
+import random
+
+secret_number = random.randint(1, 10)
+guess = int(input("guess the number from (1,10):"))
+attempts = 1
+while guess != secret_number:
+    if guess < secret_number:
+        print("Too low! try again.")
+    elif guess > secret_number:
+        print("Too high! try again.")
+    guess = int(input("guess the number again:"))
+    attempts += 1
+print("Congrats! you did it.")
+print("you found it in", attempts, "attempts.")
+print("secret number:", secret_number)

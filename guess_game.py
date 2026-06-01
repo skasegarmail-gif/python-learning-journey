bb7c3818c1619bf23c041fbaaefd008f35debf21
skasegarmail-gif <skasegar.mail@gmail.com>
2026-06-01 23:01:43 +0330

    Add attempts limit

--- guess_game.py
+++ guess_game.py
@@ -3,13 +3,18 @@ import random
 secret_number = random.randint(1, 10)
 guess = int(input("guess the number from (1,10):"))
 attempts = 1
-while guess != secret_number:
+
+while guess != secret_number and attempts < 5:
     if guess < secret_number:
         print("Too low! try again.")
     elif guess > secret_number:
         print("Too high! try again.")
-    guess = int(input("guess the number again:"))
     attempts += 1
-print("Congrats! you did it.")
-print("you found it in", attempts, "attempts.")
-print("secret number:", secret_number)
+    guess = int(input("guess the number again:"))
+
+if guess == secret_number:
+    print("Congrats! you did it.")
+    print("You found it in", attempts, "attempts.")
+else:
+    print("Game Over!")
+    print("The secret number was", secret_number)
